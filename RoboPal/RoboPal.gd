8bbbf22781aef23a1649f31adb4f49957df8dd34
Dave Ponting <pontingdavid.81@gmail.com>
2022-08-12 23:28:00 +0100

    first commit

---
+++ RoboPal/RoboPal.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 80
+const ROLL_SPEED = 120
+const ACCEL = 500
+const FRICTION = 500
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+var velocity = Vector2.ZERO
+
+func _ready():
+	animationTree.active = true
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCEL * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+	move()
+	
+func move():
+	velocity = move_and_slide(velocity)
